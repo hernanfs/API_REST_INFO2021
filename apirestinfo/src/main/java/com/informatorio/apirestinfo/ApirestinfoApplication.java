98a128554d8cd36b496fd0803d0d59dcbdb837c2
Hernán Fernandez <87276654+hernanfs@users.noreply.github.com>
2021-12-04 17:30:18 -0300

    Add files via upload

---
+++ apirestinfo/src/main/java/com/informatorio/apirestinfo/ApirestinfoApplication.java
@@ -0,0 +1,13 @@
+package com.informatorio.apirestinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestinfoApplication.class, args);
+	}
+
+}
