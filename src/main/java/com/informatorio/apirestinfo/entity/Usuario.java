087a0b4232c5c8b06b5bb8cde8dce7a265715d85
hernanfs <hernanfernandezs@gmail.com>
2021-12-05 12:40:14 -0300

    commit

--- src/main/java/com/informatorio/apirestinfo/entity/Usuario.java
+++ src/main/java/com/informatorio/apirestinfo/entity/Usuario.java
@@ -1,5 +1,51 @@
 package com.informatorio.apirestinfo.entity;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Table(name = "usuarios")
+
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String direccion;
+    private String ciudad;
+    private String pais;
+    private String codigoPostal;
+
+    @Email(regexp = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$")
+    private String email;
+    private String password;
+    private String passwordConfirm;
+
+    @CreationTimestamp //Crea una fecha de creacion automaticamente
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate fechaCreacion;
+
     
 }
